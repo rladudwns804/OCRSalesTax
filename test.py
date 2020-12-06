793845f6252bf193852f4bda679f9e23ce681a60
Paul <rladudwns804@gmail.com>
2020-12-06 04:29:24 -0600

    added googledriveapi to upload recipet image

--- test.py
+++ test.py
@@ -4,17 +4,16 @@ from tkinter import filedialog
 import pytesseract as pt
 import re
 import pandas as pd
+import googledrive
 
 
-
-#tkinter for opening image file
+#Prompt user to open image file in system:
 root = tk.Tk()
 root.withdraw()
 filename = filedialog.askopenfilename()
 im = Image.open(filename)
 
-
-
+#Using pytesseract, convert image into text:
 text = pt.image_to_string(im, lang="eng")
 text.upper()
 substring = "TAX"
@@ -25,7 +24,7 @@ newText = text[find:find+12]
 newText = re.sub("[^.0-9]",'',newText)
 amount = newText.replace(" ", "")
 
-#Load to Dataframe and export as csv:
+#Load to Dataframe and export as csv/excel:
 df = pd.DataFrame(columns = ["Tax"])
 x = 0
 
@@ -37,6 +36,7 @@ else:
     df.loc[x,"Tax"] = amount
     df.to_csv("sales tax.csv")
 
+#Upload image to google drive:
+googledrive(im)
 
 
-print(amount)
